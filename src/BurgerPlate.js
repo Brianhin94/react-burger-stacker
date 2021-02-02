085cc96950ba0edf1b21b818da61c3a9125c1625
Brianhin94 <brianhin64@gmail.com>
2021-02-02 10:32:41 -0700

    homework

---
+++ src/BurgerPlate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import BurgerStack from './BurgerStack';
+import ClearBurger from './ClearBurger';
+
+class BurgerPlate extends Component {
+  render () {
+    return (
+      <div>
+      
+      </div>
+    )
+  }
+};
+
+export default BurgerPlate;
